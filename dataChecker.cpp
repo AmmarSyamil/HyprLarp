4203529aac680db2fcf8e75f7f50dc39e50920ea
ammar syamil <ammarsyamil057@gmail.com>
2026-05-14 12:07:12 +0700

    making note so ignore ts

--- dataChecker.cpp
+++ dataChecker.cpp
@@ -1,12 +1,10 @@
+// Ignore this file, its funcionality already embeded in the socketReceive.cpp.
+
 #include <iostream>
 #include <vector>
-
 #include "socketReceive.hpp" // SocketConnection(data)
-
 #include <thread> // multithreading!!
-
 #include <string> // check letter to detect activewindow2
-
 #include <mutex> // mutex for better multithreading
 
 int main() {
---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <mutex>
+#include <vector>
+#include <thread>
+#include <unistd.h>
+
+int main() {
+    // Finding what workspace and window that this program was launch from
+
+    return 1;
+}
